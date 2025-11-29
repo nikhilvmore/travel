27ea2a23f760893426e6c67b93d1bdbba9642ee7
nikhilvmore <nikhil.v.more@gmail.com>
2025-11-29 15:48:40 +0530

    new pkg

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/japan" element={<Japan />} />
           </Routes>
         </main>
         <Footer />
